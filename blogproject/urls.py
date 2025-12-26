0e8dbc48aa23a7a3eb0cd271457d6898f5814116
Anju <anjalyjins2004@gmail.com>
2025-12-26 15:13:14 +0530

    Install dotenv and fix settings

--- blogproject/urls.py
+++ blogproject/urls.py
@@ -19,6 +19,9 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
+from django.contrib import admin
+from django.urls import path, include
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('blog.urls')),
